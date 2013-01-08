f3bc70e58cebd7e5014c6bc885511e55452e7411
James Halliday <mail@substack.net>
2013-01-08 06:58:06 -0800

    jumping creature

--- example/debris.js
+++ example/debris.js
@@ -26,3 +26,36 @@ window.addEventListener('keyup', ctrlToggle);
 var erase = true
 function ctrlToggle (ev) { erase = !ev.ctrlKey }
 game.requestPointerLock('canvas');
+
+game.addItem((function () {
+    var material = game.loadTextures([ 'obsidian' ]);
+    var mesh = new game.THREE.Mesh(
+        new game.THREE.CubeGeometry(10, 30, 10),
+        material
+    );
+    mesh.geometry.faces.forEach(function (face) {
+        face.materialIndex = 0;
+    });
+    mesh.translateX(87.5);
+    mesh.translateY(550);
+    mesh.translateZ(12.5);
+    
+    var item = {
+        mesh: mesh,
+        width: 10,
+        height: 30,
+        depth: 10,
+        collisionRadius: 20,
+        velocity: { x: 0, y: 0, z: 0 }
+    };
+    setInterval(function () {
+        if (item.velocity.y > 0.01) return;
+        item.velocity.y += 0.1;
+        item.velocity.x += (2 * Math.random() - 1) / 40;
+        item.velocity.z += (2 * Math.random() - 1) / 40;
+        mesh.translateY(5);
+        item.resting = false;
+    }, 2000);
+    
+    return item;
+})());
